16fc0a8439109276ce8fb55e428cce69fb46314c
Hans Petter Langtangen <hpl@simula.no>
2013-09-19 09:33:25 +0200

    updates

--- doc/slides/src/decay/src-decay/experiments/archived-reports/project_mathjax/doc/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Run experiment documented in report.html
-
-python decay_exper1_mathjax.py 1.25 0.75 0.5 0.1
--- doc/writing_reports/make.sh
+++ doc/writing_reports/make.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Same copy script as in hplgit.github.com/teamods/writing_reports
 
-cp -r ~/vc/deqbook/doc/src/decay/src-decay/experiments/archived-reports/* .
+cp -r ~/vc/deqbook/doc/src/archive/decay-reports/* .
 
